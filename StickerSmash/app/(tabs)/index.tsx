314474e8e867b2c37bc24f2035ab9f250f0ce2d3
tuwi-0808374 <0808374@hr.nl>
2025-04-15 12:55:51 +0200

    part 3

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
+import { Image } from 'expo-image';
+import ImageViewer from '@/components/ImageViewer';
+import Button from '@/components/Button';
+
+const PlaceHolderImage = require('../../assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceHolderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' theme='primary' />
+        <Button label='Use this photo' />
+      </View>
     </View>
   );
 }
@@ -14,14 +25,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
   },
   button: {
     fontSize: 20,
     color: '#fff',
     textDecorationLine: 'underline',
   },
+  imageContainer: {
+    flex: 1,
+  },
+  footerContainer:{
+    flex: 1 / 3,
+    alignItems: 'center',
+  }
 });
